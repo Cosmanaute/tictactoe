d746a4e4049b32c1104011fb06f3f93686d71b37
cuebin <beknosee@outlook.com>
2023-04-04 20:10:56 +0200

    Added paint-function
    Added code to the main function and formatted the output. <1> bug

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::slice::SliceIndex;
+use std::io::Write;
+use std::io::stdin;
+use std::io::stdout;
+
+fn draw_board(board: &[char; 9]) {
+    println!("{}|{}|{}", board[0], board[1], board[2]);
+    println!("{}|{}|{}", board[3], board[4], board[5]);
+    println!("{}|{}|{}", board[6], board[7], board[8]);
 }
+
+fn get_input() -> u8 {
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Error reading input!");
+    return input.parse::<i8>().unwrap();
+}
+
+fn main() {
+    
+    let mut board: [char; 9] = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    let mut turn: char = 'o';
+
+    for i in 0..10 {
+        draw_board(&board);
+        println!("{}'s turn to move: ", turn);
+        let input = get_input();
+        if board[input] == ' ' {
+           board[input] == turn;
+        }
+        else {
+            println!("Cannot move here!");
+        }
+
+    }
+
+}
+
